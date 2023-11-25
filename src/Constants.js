5b9da62add6729f6b737172fb6f6b1f638f0d579
hzzzi <hji2004@naver.com>
2023-11-25 18:34:10 +0900

    feat(Constants): 입출력 메세지, 게임 관련 상수 생성

---
+++ src/Constants.js
@@ -0,0 +1,25 @@
+const MESSAGES = {
+  Game: {
+    Start: '숫자 야구 게임을 시작합니다.',
+    Input: '숫자를 입력해주세요 :',
+    Success: '3스트라이크/n3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    RestartOrEnd: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  },
+  Result: {
+    Ball: '볼',
+    Strike: '스트라이크',
+    Nothing: '낫싱',
+  },
+};
+
+const GAME = {
+  Number: {
+    Range: {
+      Start: 1,
+      End: 9,
+    },
+    Digits: 3,
+  },
+};
+
+export { MESSAGES, GAME };
