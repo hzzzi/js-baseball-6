6b985a9653471b8f715404076623265be399fbf5
hzzzi <hji2004@naver.com>
2023-11-25 20:42:19 +0900

    feat(Computer): 사용자가 입력한 수와 컴퓨터의 수 비교 기능 구현

--- src/model/Computer.js
+++ src/model/Computer.js
@@ -17,6 +17,21 @@ class Computer {
       this.#answer = computer;
     }
   }
+
+  compareNumber(input) {
+    let ball = 0;
+    let strike = 0;
+
+    for (let i = 0; i < input.length; i += 1) {
+      if (this.#answer.includes(input[i]) && this.#answer[i] !== input[i]) {
+        ball += 1;
+      }
+      if (this.#answer.includes(input[i]) && this.#answer[i] === input[i]) {
+        strike += 1;
+      }
+    }
+    return [ball, strike];
+  }
 }
 
 export default Computer;
