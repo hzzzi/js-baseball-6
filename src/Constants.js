2d40bea68cea06b80cfac59750ded1e2986bd310
hzzzi <hji2004@naver.com>
2023-11-25 22:35:25 +0900

    fix(Constants): 불필요한 상수 삭제 및 오타 수정

--- src/Constants.js
+++ src/Constants.js
@@ -2,13 +2,9 @@ const MESSAGES = {
   Game: {
     Start: '숫자 야구 게임을 시작합니다.',
     Input: '숫자를 입력해주세요 :',
-    Success: '3스트라이크/n3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    Success: '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료',
     RestartOrEnd: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-  },
-  Result: {
-    Ball: '볼',
-    Strike: '스트라이크',
-    Nothing: '낫싱',
+    End: '게임을 종료합니다.',
   },
 };
 
