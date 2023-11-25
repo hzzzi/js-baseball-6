412920df0b159cd8508421215f8ffc827d4830d8
hzzzi <hji2004@naver.com>
2023-11-25 22:37:12 +0900

    refactor(OutputView): 결과 출력 코드 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -6,16 +6,19 @@ const OutputView = {
     Console.print(MESSAGES.Game.Start);
   },
   ball(ball) {
-    Console.print(ball + MESSAGES.Result.Ball);
+    Console.print(`${ball}볼`);
   },
   strike(strike) {
-    Console.print(strike + MESSAGES.Result.Strike);
+    Console.print(`${strike}스트라이크`);
   },
   nothing() {
-    Console.print(MESSAGES.Result.Nothing);
+    Console.print(`낫싱`);
   },
   GameSuccess() {
     Console.print(MESSAGES.Game.Success);
+  },
+  GameEnd() {
+    Console.print(MESSAGES.Game.End);
   },
 };
 
